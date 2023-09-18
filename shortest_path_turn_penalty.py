5a4b40d4705e695c58298dd128ffdc7e7e26b37b
Max Ng <maxng@u.northwestern.edu>
2023-09-18 16:37:43 -0500

    Update shortest_path_turn_penalty.py

--- shortest_path_turn_penalty.py
+++ shortest_path_turn_penalty.py
@@ -4,6 +4,8 @@ def shortest_path_turn_penalty(G, source, target, weight="travel_time", penalty=
     """
     Uses Dijkstra's algorithm to find the shortest weighted paths to one or multiple targets with turn penalty.
     This function is adapted from networkx.algorithms.shortest_paths.weighted._dijkstra_multisource.
+    The turn penalty implementation is based on:
+    Ziliaskopoulos, A.K., Mahmassani, H.S., 1996. A note on least time path computation considering delays and prohibitions for intersection movements. Transportation Research Part B: Methodological 30, 359–367. https://doi.org/10.1016/0191-2615(96)00001-X
 
 
     Parameters
