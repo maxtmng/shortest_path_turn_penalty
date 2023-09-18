fdae65961136e9e124d068792abe0c5b2b770b91
Max Ng <maxng@u.northwestern.edu>
2023-09-18 16:36:43 -0500

    Create shortest_path_turn_penalty.py

---
+++ shortest_path_turn_penalty.py
@@ -0,0 +1,115 @@
+from heapq import heappop, heappush
+from itertools import count
+def shortest_path_turn_penalty(G, source, target, weight="travel_time", penalty={}, next_node = None):
+    """
+    Uses Dijkstra's algorithm to find the shortest weighted paths to one or multiple targets with turn penalty.
+    This function is adapted from networkx.algorithms.shortest_paths.weighted._dijkstra_multisource.
+
+
+    Parameters
+    ----------
+    G : NetworkX graph
+
+    source : non-empty iterable of nodes
+        Starting nodes for paths. If this is just an iterable containing
+        a single node, then all paths computed by this function will
+        start from that node. If there are two or more nodes in this
+        iterable, the computed paths may begin from any one of the start
+        nodes.
+
+    target : node label, single node or a list
+        Ending node (or a list of ending nodes) for path. Search is halted when any target is found.
+
+    weight: function
+        Function with (u, v, data) input that returns that edge's weight
+        or None to indicate a hidden edge
+
+    penalty : dict, optional (default={})
+        Dictionary containing turn penalties. The key is a tuple (u, v, m) where
+        u, v are the nodes of the current edge and m is the next node.
+
+    next_node : node, optional (default=None)
+        Next node to consider from the source.
+
+    Returns
+    -------
+    list of nodes
+        Path from source to target.
+
+    Raises
+    ------
+    NodeNotFound
+        If the source or target is not in `G`.
+
+    ValueError
+        If contradictory paths are found due to negative weights.
+    """
+
+    if source not in G:
+        raise nx.NodeNotFound(f"Source {source} not in G")
+
+    if isinstance(target, list) and not all(t in G for t in target):
+        raise nx.NodeNotFound("One or more targets not in G")
+    elif target not in G:
+        raise nx.NodeNotFound(f"Target {target} not in G")
+
+    G_succ = G._adj  # For speed-up (and works for both directed and undirected graphs)
+    weight = nx.algorithms.shortest_paths.weighted._weight_function(G, weight)
+    push = heappush
+    pop = heappop
+    dist = {}  # dictionary of final distances
+    paths = {source: [source]}
+    target_list = [target] if not isinstance(target, list) else target
+    reached_target = None
+
+    seen = {}
+    c = count()
+    fringe = []
+
+    seen[source] = {}
+    if next_node is None:
+        for m,_ in G_succ[source].items():
+            seen[source][m] = 0
+            push(fringe, (0, next(c), source, m))
+    else:
+        push(fringe, (0, next(c), source, next_node))
+
+    while fringe:
+        (d, _, v, m) = pop(fringe)
+        u = m
+        if v in dist:
+            if u in dist[v]:
+                continue  # already searched this node.
+        else:
+            dist[v] = {}
+        dist[v][u] = d
+
+        if v in target_list:
+            reached_target = v
+            break
+
+        e = G[v][u]
+        for m in G_succ[u]:
+            cost = weight(v, u, e) 
+            if (v,u,m) in penalty:
+                cost += penalty[v,u,m]
+            
+            if cost is None:
+                continue
+            vu_dist = dist[v][u] + cost
+            if u in dist:
+                if m in dist[u]:
+                    u_dist = dist[u][m]
+                    if vu_dist < u_dist:
+                        raise ValueError("Contradictory paths found:", "negative weights?")
+            elif u not in seen or m not in seen[u] or vu_dist < seen[u][m]:
+                if u not in seen:
+                    seen[u] = {}
+                seen[u][m] = vu_dist
+                push(fringe, (vu_dist, next(c), u, m))
+                if paths is not None:
+                    paths[u] = paths[v] + [u]
+
+    # The optional predecessor and path dictionaries can be accessed
+    # by the caller via the pred and paths objects passed as arguments.
+    return paths[reached_target]
